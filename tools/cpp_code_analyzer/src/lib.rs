a4f8d79da03809c7fc070a1d95d5a755691f2eb4
Benjamin Kober <benko@kober-systems.com>
2025-06-30 18:21:42 +0300

    better error reporting

--- tools/cpp_code_analyzer/src/lib.rs
+++ tools/cpp_code_analyzer/src/lib.rs
@@ -3,9 +3,13 @@ pub mod checker;
 pub mod ast;
 
 pub fn analyze_cpp(input: &str) -> Vec<String> {
+  lints_to_strings(analyze_cpp_errors(input))
+}
+
+pub fn analyze_cpp_errors(input: &str) -> Vec<ast::LintError> {
   let ast = parser::parse_cpp_chunc(input);
 
-  lints_to_strings(checker::check_global_codechunk(ast))
+  checker::check_global_codechunk(ast)
 }
 
 fn lints_to_strings(input: Vec<ast::LintError>) -> Vec<String> {
--- tools/cpp_code_analyzer/src/main.rs
+++ tools/cpp_code_analyzer/src/main.rs
@@ -1,7 +1,11 @@
 use std::path::PathBuf;
 
 use clap::Parser;
-use cpp_code_analyzer::analyze_cpp;
+use codespan_reporting::diagnostic::{Diagnostic, Label};
+use codespan_reporting::files::SimpleFiles;
+use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
+use codespan_reporting::term;
+use cpp_code_analyzer::analyze_cpp_errors;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -14,10 +18,22 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let input = std::fs::read_to_string(args.input).expect("Could not read input");
-    let errors = analyze_cpp(&input);
+    let input = std::fs::read_to_string(&args.input).expect("Could not read input");
+    let errors = analyze_cpp_errors(&input);
+
+    let mut files = SimpleFiles::new();
+    let file_id = files.add(args.input.to_string_lossy(), input);
 
     for error in errors.iter() {
-      eprintln!("\t{error}");
+      let diagnostic = Diagnostic::error()
+          .with_message(&error.message)
+          .with_labels(vec![
+              Label::primary(file_id, error.range.start..error.range.end),
+          ]);
+
+      let writer = StandardStream::stderr(ColorChoice::Always);
+      let config = codespan_reporting::term::Config::default();
+
+      term::emit(&mut writer.lock(), &config, &files, &diagnostic).unwrap();
     }
 }
